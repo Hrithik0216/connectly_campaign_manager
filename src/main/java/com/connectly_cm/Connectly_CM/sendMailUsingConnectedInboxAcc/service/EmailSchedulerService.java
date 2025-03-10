126ecc0f95f15693b07eda74fd3d162393c12649
Hrithik0216 <hrithik3285@gmail.com>
2025-03-10 23:37:14 +0530

    Checkpoint commit. Mon night. Working good till now

--- src/main/java/com/connectly_cm/Connectly_CM/sendMailUsingConnectedInboxAcc/service/EmailSchedulerService.java
+++ src/main/java/com/connectly_cm/Connectly_CM/sendMailUsingConnectedInboxAcc/service/EmailSchedulerService.java
@@ -84,86 +84,6 @@ public class EmailSchedulerService {
     }
 
 
-//    private void scheduleEmailSequence(EmailSequence emailSequence, List<EmailSequenceStep> emailSequenceSteps) {
-//        LOGGER.info("Scheduling email sequence with " + emailSequence.toString());
-//        List<EmailSequenceStep> stepsWithSequenceId = emailSequenceSteps.stream().map(step -> {
-//            step.setSequenceId(emailSequence.getId());
-//            return step;
-//        }).toList();
-//        emailSequenceStepRepository.saveAll(stepsWithSequenceId);
-//        long delaySeconds = 0;
-//
-//        for (EmailSequenceStep step : emailSequenceSteps) {
-//            long initialDelay = step.getDelayInSeconds();
-//            scheduler.schedule(() -> {
-//                LOGGER.info("Started scheduler service for the step: " + step.toString());
-//                ZonedDateTime now = ZonedDateTime.now(ZoneId.of("Asia/Kolkata"));
-//                    boolean windowResult = TimeWindowUtils.isWithinTimeWindow(now, emailSequence.getTimeWindow());
-//                    LOGGER.info("Window result "+windowResult);
-//                if (TimeWindowUtils.isWithinTimeWindow(now, emailSequence.getTimeWindow())) {
-//                    LOGGER.info("The step is within the timewindow");
-//                    try {
-//                        if (emailSequence.getThreadId() == null) {
-//                            LOGGER.info("The thread value is null for this email sequence");
-//                            // First email in the sequence: send and get Thread ID
-//                            ResponseEntity<Object> emailResponseMap = sequenceService.sendEmailWithoutThreadId(
-//                                    emailSequence.getUserId(),
-//                                    emailSequence.getFromAddress(),
-//                                    step.getToEmailAddress(),
-//                                    step.getSubject(),
-//                                    step.getBodyText()
-//                            );
-//                            LOGGER.info("The emailResponseMap is " + emailResponseMap.toString());
-//                            LOGGER.info("THe emailresponseMap statuscode is " + emailResponseMap.getStatusCode());
-//                            if (emailResponseMap.getStatusCode() == HttpStatus.OK) {
-//                                LOGGER.info("Got success code " + emailResponseMap.getStatusCode());
-//                                HashMap<String, Object> responseBody = (HashMap<String, Object>) emailResponseMap.getBody();
-//                                String threadId = (String) responseBody.get("threadId");
-//                                LOGGER.info("The threadId is " + threadId);
-//
-//                                // Update sequence with Thread ID
-//                                emailSequence.setThreadId(threadId);
-//                                LOGGER.info("The email sequence with threadId " + emailSequence);
-//                                emailSequenceRepository.save(emailSequence);
-//                            }
-//                        } else {
-//                            LOGGER.info("The email sequence is scheduled without threadID");
-//                            // Subsequent emails: use the stored Thread ID
-//                            sequenceService.sendEmailWithThreadId(
-//                                    emailSequence.getUserId(),
-//                                    emailSequence.getFromAddress(),
-//                                    step.getToEmailAddress(),
-//                                    step.getSubject(),
-//                                    step.getBodyText(),
-//                                    emailSequence.getThreadId()
-//                            );
-//                        }
-//
-//                        // Update step as completed
-//                        step.setCompleted(true);
-//                        LOGGER.info("The completed step is " + step.toString());
-//                        emailSequenceStepRepository.save(step);
-//
-//                        // Update sequence's last_processed_at
-//                        emailSequence.setLastProcessed(new Date());
-//                        LOGGER.info("The completed sequence is " + emailSequence.toString());
-//                        emailSequenceRepository.save(emailSequence);
-//                    } catch (Exception e) {
-//                        // Handle exception (e.g., log the error)
-//                        e.printStackTrace();
-//                    }
-//                } else {
-//                    LOGGER.info("Error in window");
-//                    ResponseEntity.status(HttpStatus.CONFLICT).body("Need tp write logic for rescheduling mails");
-////                    rescheduleEmail(step, emailSequence);
-//                }
-//            }, delaySeconds + initialDelay, TimeUnit.SECONDS);
-//
-//            delaySeconds += initialDelay;
-//        }
-//    }
-
-
     private void scheduleEmailSequence2(EmailSequence emailSequence, List<EmailSequenceStep> emailSequenceSteps) {
         LOGGER.info("Scheduling email sequence with " + emailSequence.toString());
         List<EmailSequenceStep> stepsWithSequenceId = emailSequenceSteps.stream().map(step -> {
@@ -179,13 +99,12 @@ public class EmailSchedulerService {
                 LOGGER.info("Started scheduler service for the step: " + step.toString());
                 ZonedDateTime now = ZonedDateTime.now(ZoneId.of("Asia/Kolkata"));
                 boolean windowResult = TimeWindowUtils.isWithinTimeWindow(now, emailSequence.getTimeWindow());
-                LOGGER.info("Window result "+windowResult);
+                LOGGER.info("Window result " + windowResult);
                 if (TimeWindowUtils.isWithinTimeWindow(now, emailSequence.getTimeWindow())) {
-                    LOGGER.info("The step is within the timewindow");
+                    LOGGER.info("The step is within the time window");
                     try {
                         if (emailSequence.getThreadId() == null) {
                             LOGGER.info("The thread value is null for this email sequence");
-                            // First email in the sequence: send and get Thread ID
                             ResponseEntity<?> emailResponseMap = sequenceService.sendEmail(
                                     emailSequence.getUserId(),
                                     emailSequence.getFromAddress(),
@@ -194,14 +113,7 @@ public class EmailSchedulerService {
                                     step.getBodyText()
                             );
 
-//                            Map<String, Object> responseBody = (Map<String, Object>) emailResponseMap.getBody();
-//                            Map<String, Object> mailResult = (Map<String, Object>) responseBody.get("mailResult");
-//                            EmailResponse emailResponse = (EmailResponse) mailResult.get("emailResponse");
-//
-//                            String threadId = emailResponse.getThreadId();
-//                            LOGGER.info("Thread ID: " + threadId);
-
-                            LOGGER.info("THe emailresponseMap statuscode is " + emailResponseMap.getStatusCode());
+//                            LOGGER.info("THe emailresponseMap statuscode is " + emailResponseMap.getStatusCode());
                             if (emailResponseMap.getStatusCode() == HttpStatus.OK) {
                                 LOGGER.info("Got success code " + emailResponseMap.getStatusCode());
                                 Map<String, Object> responseBody = (Map<String, Object>) emailResponseMap.getBody();
@@ -210,14 +122,12 @@ public class EmailSchedulerService {
                                 String threadID = emailResponse.getThreadId();
                                 LOGGER.info("The threadId is " + threadID);
 
-                                // Update sequence with Thread ID
                                 emailSequence.setThreadId(threadID);
-                                LOGGER.info("The email sequence with threadId " + emailSequence);
+                                LOGGER.info("The email sequence without threadId " + emailSequence);
                                 emailSequenceRepository.save(emailSequence);
                             }
                         } else {
-                            LOGGER.info("The email sequence is scheduled without threadID");
-                            // Subsequent emails: use the stored Thread ID
+                            LOGGER.info("The email sequence is scheduled with threadID");
                             sequenceService.sendEmailWithThreadId(
                                     emailSequence.getUserId(),
                                     emailSequence.getFromAddress(),
@@ -228,23 +138,18 @@ public class EmailSchedulerService {
                             );
                         }
 
-                        // Update step as completed
                         step.setCompleted(true);
                         LOGGER.info("The completed step is " + step.toString());
                         emailSequenceStepRepository.save(step);
-
-                        // Update sequence's last_processed_at
                         emailSequence.setLastProcessed(new Date());
                         LOGGER.info("The completed sequence is " + emailSequence.toString());
                         emailSequenceRepository.save(emailSequence);
                     } catch (Exception e) {
-                        // Handle exception (e.g., log the error)
-                        e.printStackTrace();
+                        LOGGER.error("Error in sending mail" + e.getMessage());
                     }
                 } else {
                     LOGGER.info("Error in window");
                     ResponseEntity.status(HttpStatus.CONFLICT).body("Need tp write logic for rescheduling mails");
-//                    rescheduleEmail(step, emailSequence);
                 }
             }, delaySeconds + initialDelay, TimeUnit.SECONDS);
 
