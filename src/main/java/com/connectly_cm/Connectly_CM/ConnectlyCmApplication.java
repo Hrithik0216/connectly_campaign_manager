e2d1071e31c413cc6f78b5d141d98fe39958ed25
Hrithik0216 <hrithik3285@gmail.com>
2025-04-13 13:04:11 +0530

    Added AES/GCM encryption to secure refresh and access tokens for pipedrive and other new token which will be generated in future

--- src/main/java/com/connectly_cm/Connectly_CM/ConnectlyCmApplication.java
+++ src/main/java/com/connectly_cm/Connectly_CM/ConnectlyCmApplication.java
@@ -1,16 +1,20 @@
 package com.connectly_cm.Connectly_CM;
 
+import com.connectly_cm.Connectly_CM.EncryptionUtils.EncryptionAes.EncryptionAes;
 import org.apache.log4j.PropertyConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collection;
+import java.util.Collections;
+
 @SpringBootApplication
 public class ConnectlyCmApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConnectlyCmApplication.class, args);
-		String log4jConfPath = "/home/hrithik/Desktop/Connectly/connectly_campaign_manager/src/main/resources/log4j.properties";
-		PropertyConfigurator.configure(log4jConfPath);
-	}
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ConnectlyCmApplication.class, args);
+        String log4jConfPath = "/home/hrithik/Desktop/Connectly/connectly_campaign_manager/src/main/resources/log4j.properties";
+        PropertyConfigurator.configure(log4jConfPath);
+    }
 
 }
