b2885909270e96538b4c6edf1aa9b67f5e9d34a7
Hrithik0216 <hrithik3285@gmail.com>
2025-05-04 01:27:31 +0530

    Refracted few code

--- src/main/java/com/connectly_cm/Connectly_CM/Services/users/UserService.java
+++ src/main/java/com/connectly_cm/Connectly_CM/Services/users/UserService.java
@@ -1,15 +1,10 @@
 package com.connectly_cm.Connectly_CM.Services.users;
 
-import com.connectly_cm.Connectly_CM.ErrResponses.UserDataErrResponse;
-import com.connectly_cm.Connectly_CM.constants.CustomErrCode;
 import com.connectly_cm.Connectly_CM.utils.StringUtils.StringUtil;
 import com.connectly_cm.Connectly_CM.models.users.User;
 import com.connectly_cm.Connectly_CM.repositories.userRepository.UserRepository;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
--- src/main/java/com/connectly_cm/Connectly_CM/constants/CustomErrCode.java
+++
@@ -1,8 +0,0 @@
-package com.connectly_cm.Connectly_CM.constants;
-
-public class CustomErrCode {
-    public static final String UNAUTHORIZED_USER="AUTH_4001";
-    public static final String JWT_TOKEN_EXPIRED="AUTH_4002";
-    public static final String MISSING_JWT_TOKEN="AUTH_4003";
-    public static final String USER_DETAILS="AUTH_2000";
-}
